df01e450dccdda5caa2a737f93b9b5b2cea4f95f
roaris <one0803kyopro@gmail.com>
2024-06-29 12:42:25 +0900

    関数呼び出し

--- object/environment.go
+++ object/environment.go
@@ -2,6 +2,7 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
@@ -11,10 +12,19 @@ func NewEnvironment() *Environment {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
